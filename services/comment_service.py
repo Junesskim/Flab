6e4c89e9c0fa36db53fe08d34a9ad9fec20a3004
Junesskim <aktpfkxl9891@naver.com>
2023-11-27 22:31:53 +0900

    Add files in service, domain folders

---
+++ services/comment_service.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timezone, timedelta
+from typing import List, Optional
+
+from sqlmodel import Session
+
+from domain.models import Comment
+
+seoul_timezone = timezone(timedelta(hours=9))
+
+def create_comment(session: Session, comment: Comment) -> None:
+    current_time = datetime.now(tz=seoul_timezone)
+    comment.created_at = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    session.add(comment)
+    session.commit()
+    session.refresh(comment)
+
+def get_comment_by_id(session: Session, comment_id: int) -> Optional[Comment]:
+    return session.get(Comment, comment_id)
+
+def get_comments_by_author_id(session: Session, author_id: int) -> List[Comment]:
+    return session.query(Comment).filter(Comment.author_id == author_id).all()
+
+def get_comments_by_post_id(session: Session, post_id: int) -> List[Comment]:
+    return session.query(Comment).filter(Comment.post_id == post_id).all()
