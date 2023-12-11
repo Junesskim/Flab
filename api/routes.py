ed3bb8414bf1921eff31c28603270ebc1a29665f
Junesskim <aktpfkxl9891@naver.com>
2023-12-11 22:26:48 +0900

    updated repo

--- api/routes.py
+++ api/routes.py
@@ -34,32 +34,46 @@ def create_post_api(request: CreatePostRequest, session: Session = Depends(get_s
         created_at=post.created_at
     )
 
+class GetAllPostsResponse(SQLModel):
+    data : list[Post]
+
 @router.get("/posts/", response_model=List[Post], status_code=status.HTTP_200_OK)
-def get_all_posts_api(session: Session = Depends(get_session)):
-    return get_all_posts(session)
+def get_all_posts_api(session: Session = Depends(get_session)) -> GetAllPostsResponse:
+    posts = get_all_posts(session)
+    return GetAllPostsResponse(
+        data= posts
+    )
+
+class GetPostResponse(SQLModel):
+    data : Post
 
 @router.get("/posts/", response_model=Post, status_code=status.HTTP_200_OK)
-def get_post(session: Session = Depends(get_session)):
-    return get_post(session)
+def get_post(session: Session = Depends(get_session)) -> GetPostResponse:
+    post = get_post(session)
+    return GetPostResponse(
+        data=post
+    )
+
 
 @router.put("/posts/{post_id}", response_model=Post, status_code=status.HTTP_200_OK)
 def update_post_api(post_id: int, updated_post: Post, session: Session = Depends(get_session)):
     post = update_post(session, post_id, updated_post)
     if post is None:
-        raise HTTPException(status_code=404, detail="게시글을 찾을 수 없습니다.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="게시글을 찾을 수 없습니다.")
     return post
 
 @router.patch("/posts/{post_id}", response_model=Post, status_code=status.HTTP_200_OK)
 def patch_post_api(post_id: int, updated_fields: dict, session: Session = Depends(get_session)):
     post = patch_post(session, post_id, updated_fields)
     if post is None:
-        raise HTTPException(status_code=404, detail="게시글을 찾을 수 없습니다.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="게시글을 찾을 수 없습니다.")
+    return post
 
 @router.delete("/posts/{post_id}", response_model=Post, status_code=status.HTTP_200_OK)
 def delete_post_api(post_id: int, session: Session = Depends(get_session)):
     post = delete_post(session, post_id)
     if post is None:
-        raise HTTPException(status_code=404, detail="게시글을 찾을 수 없습니다.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="게시글을 찾을 수 없습니다.")
     return post
 
 @router.post("/users/", response_model=User, status_code=status.HTTP_201_CREATED)
@@ -71,7 +85,7 @@ def create_user_api(user: User, session: Session = Depends(get_session)):
 def get_user_by_id_api(user_id: int, session: Session = Depends(get_session)):
     user = get_user_by_id(session, user_id)
     if user is None:
-        raise HTTPException(status_code=404, detail="게시글을 찾을 수 없습니다.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="게시글을 찾을 수 없습니다.")
     return user
 
 @router.get("/users/", response_model=List[User], status_code=status.HTTP_200_OK)
